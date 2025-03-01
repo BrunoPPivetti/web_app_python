91a3dadf579d1fbf82a7a38c0abfde5276647d59
brunoppivetti <brunopivetti2004@gmail.com>
2025-03-01 20:01:26 -0300

    dataset carregado

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Olá, mundo")
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("dataset/customer reviews.csv")
+df_top100_books = pd.read_csv("dataset/Top-100 Trending Books.csv")
+
+df_reviews
